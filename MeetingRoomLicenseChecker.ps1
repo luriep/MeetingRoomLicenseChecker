9ae8f9e2f0f1b5647df4d48f9b235695ab4d0440
luriep <pete@luriep.com>
2022-05-20 13:36:09 -0400

    Add files via upload
    Added more error checking in case there are 0 room mailboxes in an account
    Added more status messages

--- MeetingRoomLicenseChecker.ps1
+++ MeetingRoomLicenseChecker.ps1
@@ -31,6 +31,7 @@ Version 0.02:  Added error checking and status reporting. Changed to use the cur
 Version 0.03:  Added support for both Enterprise Premium and Enterprise Pack licenses
 Version 0.04:  Fixed spelling & grammar. 
 Version 0.05:  Reformatted output to break it up by license types
+Version 0.06:  Updated to show progress status in checking licenses
 
 #>
 
@@ -46,12 +47,19 @@ None
 .NOTES
 author: Peter Lurie
 created: 2022-05-10
-editied: 2022-05-11
+editied: 2022-05-20
 Note this will not properly pick up MTR-Premium licenses.  To do for future. 
 
 #>
 
 Clear-Host
+Write-Host "Welcome to Meeting Room License Checker." 
+Write-Host "This tool will look through your Exchange Online and AAD to find Room Mailbox UPNs. It will then report which rooms have MTR license, which have no license, and which have some other licenses" 
+Write-Host
+
+
+
+
 
 #Setup for AAD & ExchangeOnLine V2 EXO V2
 Write-Host "Getting ready to connect to AzureAD" 
@@ -88,7 +96,7 @@ Else
 	
 Try
 	{
-		$Prompt_EXOCreds = Connect-ExchangeOnline
+		$Prompt_EXOCreds = Connect-ExchangeOnline  -ShowBanner:$false
 		write-host "Connected successfully to your Exchange Online"
 	}
 Catch
@@ -109,14 +117,19 @@ Write-Host
 Write-Host "Searching for Rooms with licenses..."   #For a list of Product names and service plan identifiers for licensing, see https://docs.microsoft.com/en-us/azure/active-directory/enterprise-users/licensing-service-plan-reference
 
 
+$i,$x = 0,$Room_UPNs.count   #Setup for counting devices
+if ($x -eq $null) {$x = 1}   #run through the loop at least once to print results, otherwise will get a divide/0 error
 ForEach ($UPN in $Room_UPNs){
+    Write-Progress -activity "Searching for Rooms with licenses..." -status "Scanned: $i of $x" -PercentComplete ((($i++)/ $x) * 100)
     $UPN_license =  Get-AzureADUserLicenseDetail -ObjectID $UPN.ExternalDirectoryObjectId | Select-Object -ExpandProperty SkuPartNumber
     
     $temp = [pscustomobject]@{'DisplayName'=$UPN.DisplayName;'UPN'=$UPN.PrimarySmtpAddress; 'Licenses'=$UPN_license}
-    if ($null -eq $UPN_license) {$No_License.add($temp) | Out-Null}
-    if ("MEETING_ROOM" -in $UPN_license) {$MeetingRoom_License.add($temp) | Out-Null}
-    if (("MEETING_ROOM" -notin $UPN_license) -and ($null -ne $UPN_license)) {$Non_MeetingRoom_License.add($temp) | Out-Null}
+    if ($null -eq $UPN_license) {$No_License.add($temp) | Out-Null}  #find rooms without licenses
+    if ("MEETING_ROOM" -in $UPN_license) {$MeetingRoom_License.add($temp) | Out-Null}   #find rooms with meeting room standard licenses
+    if (("MEETING_ROOM" -notin $UPN_license) -and ($null -ne $UPN_license)) {$Non_MeetingRoom_License.add($temp) | Out-Null}  #Check to make build other license list
     $temp = $null
+     
+
 }
 
 Write-Host $Non_MeetingRoom_License.count "Rooms with Non-MTR licenses." -ForegroundColor Cyan
